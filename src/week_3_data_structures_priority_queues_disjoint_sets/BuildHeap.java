329cfa70f73f95d92e6031380a12d2cbfaa5e71f
Saurabh <vaibhav.pawar@thinkinghut.in>
2020-04-16 15:12:14 +0530

    coding challenges.

---
+++ src/week_3_data_structures_priority_queues_disjoint_sets/BuildHeap.java
@@ -0,0 +1,155 @@
+package week_3_data_structures_priority_queues_disjoint_sets;
+
+import java.io.*;
+import java.util.*;
+
+public class BuildHeap {
+    private int[] data;
+    private List<Swap> swaps;
+
+    private FastScanner in;
+    private PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        new BuildHeap().solve();
+    }
+
+    private void readData() throws IOException {
+        int n = in.nextInt();
+        data = new int[n];
+        for (int i = 0; i < n; ++i) {
+            data[i] = in.nextInt();
+        }
+    }
+
+    private void writeResponse() {
+        out.println(swaps.size());
+        for (Swap swap : swaps) {
+            out.println(swap.index1 + " " + swap.index2);
+        }
+    }
+
+    private void generateSwaps() {
+        swaps = new ArrayList<Swap>();
+
+        // this is the maximal index in data[]
+        int max_index = data.length - 1;
+
+        // this is the 0-based index of the first element with at least one child
+        int n = data.length / 2 - 1;
+
+        // this iterates backwards through all the parent nodes in data[]
+        for (int i = n; i >= 0; i--) {
+
+//            // print the index and the value of the parent node data[i]
+//            out.println(i + ": " + data[i]);
+
+            // left child node index and value
+            int l_child_index = 2 * i + 1;
+            int l_child = Integer.MAX_VALUE;
+            if (l_child_index <= max_index) {
+                l_child = data[l_child_index];
+            }
+
+            // right child node index and value
+            int r_child_index = l_child_index + 1; //2 * i + 2;
+            int r_child = Integer.MAX_VALUE;
+            if (r_child_index <= max_index) {
+                r_child = data[r_child_index];
+            }
+
+//            // print the index and the value of the left child of data[i]
+//            if (l_child != Integer.MAX_VALUE) {
+//                out.println("l_child " + l_child_index + ": " + l_child);
+//            }
+//
+//            // print the index and the value of the right child of data[i]
+//            if (r_child != Integer.MAX_VALUE) {
+//                out.println("r_child " + r_child_index + ": " + r_child);
+//            }
+
+            // minimal child value
+            int min_child = Math.min(l_child, r_child);
+
+            // if there were children (there always are, but let's check just in case)
+            // and the minimal child value is less than the value of the parent node
+            // then sift the parent node down
+            if (min_child != Integer.MAX_VALUE && min_child < data[i]) {
+                SiftDown(i);
+            }
+        }
+    }
+
+    private void SiftDown(int i) {
+//        // print the index of the maximal node
+//        out.println("SiftDown(" + i + ")");
+
+        // this is the index of the current maximal node in need to be sifted down
+        int max_index = i;
+        // this is the size of data[]
+        int size = data.length - 1;
+
+        // right child node index (first check the greater of two indexes)
+        int r_child_index = 2 * i + 2;
+        if (r_child_index <= size && data[r_child_index] < data[max_index]) {
+            max_index = r_child_index;
+        }
+
+        // left child node index (then check the lesser of two indexes)
+        int l_child_index = r_child_index - 1; //2 * i + 1;
+        if (l_child_index <= size && data[l_child_index] < data[max_index]) {
+            max_index = l_child_index;
+        }
+
+        if (i != max_index) {
+            // swap the nodes
+            swaps.add(new Swap(i, max_index));
+            int temp = data[i];
+            data[i] = data[max_index];
+            data[max_index] = temp;
+
+            // check if the newly sifted down node needs to be further sifted down
+            SiftDown(max_index);
+        }
+    }
+
+    public void solve() throws IOException {
+        in = new FastScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        readData();
+        generateSwaps();
+        writeResponse();
+        out.close();
+    }
+
+    static class Swap {
+        int index1;
+        int index2;
+
+        public Swap(int index1, int index2) {
+            this.index1 = index1;
+            this.index2 = index2;
+        }
+    }
+
+    static class FastScanner {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public FastScanner() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+        }
+
+        public String next() throws IOException {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+}
---
+++ src/week_3_data_structures_priority_queues_disjoint_sets/JobQueue.java
@@ -0,0 +1,264 @@
+package week_3_data_structures_priority_queues_disjoint_sets;
+import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class JobQueue {
+    private int numWorkers;
+    private int[] jobs;
+
+    private int[] assignedWorker;
+    private long[] startTime;
+
+    private FastScanner in;
+    private PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        new JobQueue().solve();
+    }
+
+    private void readData() throws IOException {
+        numWorkers = in.nextInt();
+        int m = in.nextInt();
+        jobs = new int[m];
+        for (int i = 0; i < m; ++i) {
+            jobs[i] = in.nextInt();
+        }
+    }
+
+    private void writeResponse() {
+        for (int i = 0; i < jobs.length; ++i) {
+            out.println(assignedWorker[i] + " " + startTime[i]);
+        }
+    }
+
+    class Worker {
+        public Worker(int id, long nextFreeTime) {
+            this.id = id;
+            this.nextFreeTime = nextFreeTime;
+        }
+
+        public int id;
+        public long nextFreeTime;
+    }
+
+    class WorkerComparator implements Comparator<Worker> {
+        @Override
+        public int compare(Worker o1, Worker o2) {
+            if (o1.nextFreeTime > o2.nextFreeTime) {
+                return 1;
+            } else if (o1.nextFreeTime < o2.nextFreeTime) {
+                return -1;
+            } else {
+                if (o1.id > o2.id) {
+                    return 1;
+                } else if (o1.id < o2.id) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+    }
+
+    private void assignJobs() {
+        // TODO: replace this code with a faster algorithm.
+        assignedWorker = new int[jobs.length];
+        startTime = new long[jobs.length];
+
+        // a built-in priority queue implementation
+        // with the comparator providing the min-heap functionality
+        PriorityQueue<Worker> pq = new PriorityQueue<>(numWorkers, new WorkerComparator());
+
+        // a current time counter to use when creating the new threads
+        long currentTime = 0;
+
+        // a best available thread
+        Worker bestThread;
+
+        for (int i = 0; i < jobs.length; i++) {
+            int heapSize = pq.size();
+
+            if (heapSize == numWorkers) {
+                // new worker threads are not available; polling the queue
+                bestThread = pq.poll();
+            } else {
+                // new worker threads are still available;
+                // deciding whether to use one of them
+                bestThread = pq.peek();
+
+                if (bestThread == null) {
+                    // creating a new worker thread because the queue is empty
+                    bestThread = new Worker(heapSize, currentTime);
+                } else if (bestThread.nextFreeTime > currentTime) {
+                    // creating a new worker thread because the existing thread
+                    // at the top of the queue is less optimal
+                    bestThread = new Worker(heapSize, currentTime);
+                } else {
+                    // using an existing worker thread from the top of the queue
+                    bestThread = pq.poll();
+                }
+            }
+
+            // read the next free time of a best available thread
+            long bestNextFreeTime = bestThread.nextFreeTime;
+
+            // write down the output values
+            assignedWorker[i] = bestThread.id;
+            startTime[i] = bestNextFreeTime;
+
+            // move the current time
+            currentTime += bestNextFreeTime;
+
+            // compute the next free time
+            bestThread.nextFreeTime = bestNextFreeTime + jobs[i];
+
+            // place the best available thread back into the queue
+            pq.add(bestThread);
+        }
+    }
+
+    // This is the best array-based implementation
+    // of the min-heap queue I could come up with
+    // in the time I had, and it still returns something like
+    // "Failed case #9/28: time limit exceeded (Time used: 6.63/4.00".
+    // Resorting to the built-in implementation of PriorityQueue<>.
+    private void assignJobs_Array() {
+        // TODO: replace this code with a faster algorithm.
+        assignedWorker = new int[jobs.length];
+        startTime = new long[jobs.length];
+
+        if (numWorkers > jobs.length) {
+            numWorkers = jobs.length;
+        }
+
+        // a min-heap of worker threads
+        Worker[] workerHeap = new Worker[numWorkers];
+
+        // a max index of a head element in a heap
+        int maxHeapHead = numWorkers - 1;
+        // an index of a head element in a heap
+        int heapHead = maxHeapHead;
+
+        // a current time counter to use when creating the new threads
+        long currentTime = 0;
+
+        // initialize the head of the heap
+        workerHeap[heapHead] = new Worker(0, currentTime);
+
+        // a best available thread
+        Worker bestThread;
+
+        for (int i = 0; i < jobs.length; i++) {
+            if (heapHead == 0) {
+                // new worker threads are not available;
+                // using the thread at the top of the queue
+                bestThread = workerHeap[0];
+            } else {
+                // new worker threads are still available;
+                // deciding whether to use one of them
+
+                if (workerHeap[heapHead].nextFreeTime > currentTime) {
+                    // creating a new worker thread because the existing thread
+                    // at the top of the queue is less optimal
+                    heapHead--;
+                    bestThread = new Worker(maxHeapHead - heapHead, currentTime);
+                    workerHeap[heapHead] = bestThread;
+                } else {
+                    // using an existing worker thread from the top of the queue
+                    bestThread = workerHeap[heapHead];
+                }
+            }
+
+            // read the next free time of a best available thread
+            long bestNextFreeTime = bestThread.nextFreeTime;
+
+            // write down the output values
+            assignedWorker[i] = bestThread.id;
+            startTime[i] = bestNextFreeTime;
+
+            // move the current time
+            currentTime += bestNextFreeTime;
+
+            // compute the next free time
+            bestThread.nextFreeTime = bestNextFreeTime + jobs[i];
+
+            // don't do SiftDown() if there is only one worker thread in the queue
+            if (heapHead == maxHeapHead) {
+                continue;
+            }
+
+            // rearrange the threads in the min-heap
+            SiftDown(workerHeap, heapHead);
+        }
+    }
+
+    private void SiftDown(Worker[] workerHeap, int heapHead) {
+        int i = heapHead;
+
+        while (true) {
+            if (i == numWorkers - 1) {
+                return;
+            } else {
+                // what follows is basically a code to compare two worker threads
+                // interspersed with the code to swap two elements of the min-heap
+                int i_id = workerHeap[i].id;
+                long i_nextFreeTime = workerHeap[i].nextFreeTime;
+
+                int i_next_id = workerHeap[i + 1].id;
+                long i_next_nextFreeTime = workerHeap[i + 1].nextFreeTime;
+
+                if (i_nextFreeTime > i_next_nextFreeTime) {
+                    workerHeap[i].id = i_next_id;
+                    workerHeap[i].nextFreeTime = i_next_nextFreeTime;
+
+                    workerHeap[++i].id = i_id;
+                    workerHeap[i].nextFreeTime = i_nextFreeTime;
+                } else if (i_nextFreeTime < i_next_nextFreeTime) {
+                    return;
+                } else {
+                    if (i_id > i_next_id) {
+                        workerHeap[i].id = i_next_id;
+                        workerHeap[i].nextFreeTime = i_next_nextFreeTime;
+
+                        workerHeap[++i].id = i_id;
+                        workerHeap[i].nextFreeTime = i_nextFreeTime;
+                    } else {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void solve() throws IOException {
+        in = new FastScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        readData();
+        assignJobs();
+        writeResponse();
+        out.close();
+    }
+
+    static class FastScanner {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public FastScanner() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+        }
+
+        public String next() throws IOException {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+}
